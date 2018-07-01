c9ca4e3afc13731849d57c68c0ee4c4e4a12ad42
chalermchai <teammiza@hotmail.com>
2018-07-01 12:42:27 +0700

    แก้ไขport

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+var port = process.env.PORT || 3000;
+server.listen(3001, () => {
 	console.log('JSON Server is running');
 });
